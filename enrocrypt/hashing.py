53771471b85ece7afd099846b859ac5aad644edf
Morgan-Phoenix <73711602+Morgan-Phoenix@users.noreply.github.com>
2022-05-24 20:30:39 +0530

    removed passlib

--- enrocrypt/hashing.py
+++ enrocrypt/hashing.py
@@ -1,5 +1,4 @@
 import hashlib, base64, uuid
-from passlib.handlers.pbkdf2 import pbkdf2_sha256
 from cryptography.hazmat.primitives import hashes
 from typing import Any
 class Hashing():
@@ -59,27 +58,6 @@ class Hashing():
                 salting2 = salting2.decode()[0:self.byt]
             return salting2
 
-    def __Salt_PBKDF(self,data):
-        salts = []
-        dataset = [] # len([]) == 7 TRUE
-        count = 0
-        salts.append(str(hashlib.sha256(uuid.uuid4().bytes).digest()).split("'")[1])
-        datas = str(data).split("/") #len([]) == 4 TRUE
-        for i in datas:
-            dataset.append(str(i).split("$"))
-            count+=1
-        dataset.pop(0)
-        dataset.pop(1)
-        for i in range(count):
-            salts.append(str(hashlib.sha256(uuid.uuid4().bytes).digest()).split("'")[1])
-            salts.append(str(hashlib.sha256(uuid.uuid4().bytes).digest()).split("'")[1])
-            salts.append(str(dataset[i]))
-        salts.append(str(hashlib.sha256(uuid.uuid4().bytes).digest()).split("'")[1])
-        salting = base64.standard_b64encode(bytes((str(base64.urlsafe_b64encode(bytes(''.join(salts).encode()))).split("'")[1]+str(base64.urlsafe_b64encode(base64.standard_b64encode((bytes(''.join(salts).encode()))))).split("'")[1]).encode()))
-        if len(salting) > self.byt:
-            salting = salting.decode()[0:self.byt]
-        return salting
-
     def SHA256(self,data:str):
         sha = hashlib.sha256(bytes(data.encode()))
         hash = sha.digest()
@@ -104,6 +82,3 @@ class Hashing():
         a = hashes.Hash(hashes.BLAKE2s(32))
         a.update(data)
         return self.__Salt(a.finalize(),salt=self.salt)
-        
-    def PBKDF2_SHA256(self,data:bytes):
-        return self.__Salt_PBKDF(pbkdf2_sha256.hash(str(data.decode)))
